073d3b5cf391fd503e1c891a26a58e00ba845981
hidexir <hidexir@gmail.com>
2019-06-12 23:57:21 +0900

    refactor

--- main.go
+++ main.go
@@ -11,28 +11,24 @@ func main() {
 	for i := 0; i < 100; i++ {
 		re := wightpick()
 		r = append(r, re)
-	} 
+	}
 	fmt.Println(r)
 }
 
-func wightpick() int{
+func wightpick() int {
 	var totalWeight int
-	data := []int{1,99}
+	data := []int{1, 99}
 	for _, value := range data {
 		totalWeight += value
 	}
-	// fmt.Printf("weight is %d\n", totalWeight)
 	rand.Seed(time.Now().UnixNano())
 	random := rand.Int() % totalWeight
-	// fmt.Printf("random is %d\n", random)
 	var pick int
 	for key, value := range data {
 		if random < value {
 			pick = key
 			break
 		}
-		random -= value
 	}
-	// fmt.Printf("result is %d\n", pick)
 	return pick
 }
